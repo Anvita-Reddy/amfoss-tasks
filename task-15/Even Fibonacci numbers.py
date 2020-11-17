9724bbbc16e693e25d9c564654163f533f9518e1
Anvita <73389089+Anvita-Reddy@users.noreply.github.com>
2020-11-18 01:47:42 +0530

    commit

---
+++ task-15/Even Fibonacci numbers.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import sys
+
+def fibonacci(n):
+    a,b=0,1
+    res=[]
+    while(b<n):
+        res.append(b)
+        a,b=b,a+b
+    del res[0]
+    return res
+
+t=int(input())
+for i in range(t):
+    N=int(input())
+    nums=fibonacci(N)
+    c=0
+    for i in nums:
+        if((i%10)%2==0):
+            c+=i
+    print(c)
