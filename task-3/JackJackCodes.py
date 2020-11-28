51471ac1122157ed0190f338542ba2f06dadf85b
Anvita Reddy <anvitainture@gmail.com>
2020-11-29 00:03:17 +0530

    commit

--- task-3/JackJackCodes.py
+++ task-3/JackJackCodes.py
@@ -1,3 +1,60 @@
+'''
+Jack Jack Codes
+
+Jack Jack has just got his superpowers.
+His parents have decided that it is high time for him to be enrolled in a Programming Course
+so that he can use his superpowers for the betterment of Open Source.
+Jack Jack unfortunately does not know how to code.
+So help Jack Jack solve the following question.
+Given an array of N integers.
+Find out if there exists atleast a pair of elements such that the sum of the elements is equal to M.
+
+Formally find out if there exist two indices i,j (i != j) such that array[i] + array[j] is equal to M. If there exists atleast one such pair print "True" else print "False"(without the quotes)
+
+Input Format
+
+The first line contains two integers N and M.
+
+The second line contains N seperated integers denoting the values of the array
+
+Constraints
+
+2 <= N <= 10^5
+
+The values of the array lie in the range [-10^7,10^7]
+
+-10^8 <= M <= 10^8
+
+Output Format
+
+Output a single line containing "True" if there exists a pair. Else output "False".
+
+Sample Input 0
+
+4 10
+4 8 2 6
+
+Sample Output 0
+
+True
+
+Explanation 0
+
+The pair [4,6] satisfies the condition (4 + 6 = 10). So the answer is "True". The pair [8,2] also satisfies the condition
+Sample Input 1
+
+3 5
+-5 2 1
+
+Sample Output 1
+
+False
+
+Explanation 1
+
+None of the pairs [-5,2], [-5,1], [2,1] satisfy the given condition so the answer is "False"
+'''
+
 def sumPair(lst,s):
     c=0
     low=0
--- task-3/TheD.py
+++ task-3/TheD.py
@@ -1,3 +1,47 @@
+'''
+Alex likes letters.
+He loves the letter ‘D’.
+He loves the letter ‘D’ especially when it comes embedded in crystals in a certain pattern.
+A crystal of size n (n is odd and n > 1) is an n × n matrix made of ‘D’ and ‘*’.
+You are given an odd integer n. Make a crystal of size n and print it.
+Look at the examples to understand what you need to print.
+
+Input Format
+
+There is only input ‘n’ denoting the size of the crystal
+
+Constraints
+
+1 < n < 30
+
+Output Format
+
+Output the pattern of the crystal.
+
+Sample Input 0
+
+3
+
+Sample Output 0
+
+*D*
+DDD
+*D*
+
+Sample Input 1
+
+5
+
+Sample Output 1
+
+**D**
+*DDD*
+DDDDD
+*DDD*
+**D**
+
+'''
+
 n=int(input())
 mid=n//2+1
 for i in range(1,n+1):
