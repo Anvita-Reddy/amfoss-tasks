4b2d5bc544b86b08ed59f785ea06b80ba5f5fbe4
Anvita Reddy <anvitainture@gmail.com>
2020-11-30 15:00:19 +0530

    commit

--- task-15/Largest-Palindrome.py
+++ task-15/Largest-Palindrome.py
@@ -1,5 +1,40 @@
-# Project Euler #4: Largest palindrome product
+'''
+Project Euler #4: Largest palindrome product
 
+A palindromic number reads the same both ways.
+The smallest 6 digit palindrome made from the product of two 3-digit numbers is 101101 = 143 x 707
+Find the largest palindrome made from the product of two 3-digit numbers which is less than N.
+
+Input Format
+First line contains T that denotes the number of test cases.
+This is followed by T lines, each containing an integer, N.
+
+Constraints
+
+1 ≤ T ≤ 100
+101101 < N < 1000000
+
+Output Format
+
+Print the required answer for each test case in a new line.
+
+Sample Input 0
+
+2
+101110
+800000
+
+Sample Output 0
+
+101101
+793397
+
+Explanation 0
+
+101101 is product of 143 and 707.
+793397 is product of 869 and 913.
+
+'''
 import sys
 
 def isPalindrome(n):
--- task-15/Largest-Prime-Factor.py
+++ task-15/Largest-Prime-Factor.py
@@ -1,4 +1,39 @@
-# Project Euler #3: Largest prime factor
+'''
+Project Euler #3: Largest prime factor
+
+The prime factors of  13195 are 5,7,13 and 29.
+What is the largest prime factor of a given number N?
+
+Input Format
+
+First line contains T, the number of test cases.
+This is followed by T lines each containing an integer N.
+
+Constraints
+
+1 ≤ T ≤ 10
+10 ≤ N ≤ 10^12
+
+Output Format
+
+For each test case, display the largest prime factor of N.
+
+Sample Input 0
+
+2
+10
+17
+
+Sample Output 0
+
+5
+17
+
+Explanation 0
+
+Prime factors of 10 are {2,5}, largest is 5.
+Prime factor of 17 is 17 itself, hence largest is 17.
+'''
 
 import sys
 import math
